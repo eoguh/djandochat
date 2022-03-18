1e75654faaf9309d61c8c54777b57ab464d2c155
Emmanuel Oguh <e.oguh@yahoo.com>
2022-03-18 16:38:05 +0100

    main

--- room/consumers.py
+++ room/consumers.py
@@ -8,7 +8,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         await self.channel_layer.group_add(
-            self.room_name,
+            self.room_name, 
             self.channel_name,
         )
         await self.accept()
